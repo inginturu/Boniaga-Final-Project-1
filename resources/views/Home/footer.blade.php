c32a82b1a943dabae9bee269fdd0cd81faee2a17
depedro-1 <depedrompane@gmail.com>
2025-04-22 08:14:15 +0700

    footer update
    okie

--- resources/views/Home/footer.blade.php
+++ resources/views/Home/footer.blade.php
@@ -1,154 +1,43 @@
-<footer class="footer pt-5 mt-5" style="background-color: black; color: white;">
+<footer class="bg-black text-white pt-4 mt-5"  style="background-color: #000;">
     <div class="container">
-        <div class=" row">
-            <div class="col-md-3 mb-4 ms-auto">
-                <div>
-                    <a href="https://www.creative-tim.com/product/material-kit">
-                        <img src="{{url('assets/img/BoniagaLogo.jpg')}}" class="mb-3 footer-logo" alt="main_logo">
-                    </a>
-                    <h6 class="font-weight-bolder mb-4" style="color: white;">Boniaga</h6>
-                </div>
-                <div>
-                    <ul class="d-flex flex-row ms-n3 nav">
-                        <li class="nav-item">
-                            <a class="nav-link pe-1" href="https://www.facebook.com/CreativeTim" target="_blank">
-                                <i class="fab fa-facebook text-lg opacity-8"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link pe-1" href="https://twitter.com/creativetim" target="_blank">
-                                <i class="fab fa-twitter text-lg opacity-8"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link pe-1" href="https://dribbble.com/creativetim" target="_blank">
-                                <i class="fab fa-dribbble text-lg opacity-8"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link pe-1" href="https://github.com/creativetimofficial" target="_blank">
-                                <i class="fab fa-github text-lg opacity-8"></i>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link pe-1" href="https://www.youtube.com/channel/UCVyTG4sCw-rOvB9oHkzZD1w"
-                                target="_blank">
-                                <i class="fab fa-youtube text-lg opacity-8"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+        <div class="row align-items-start text-center text-md-start">
+            <!-- Logo -->
+            <div class="col-md-3 mb-4">
+                <img src="{{ url('assets/img/logo.png') }}" alt="Boniaga Logo" style="max-width: 100px;">
             </div>
-            <div class="col-md-2 col-sm-6 col-6 mb-4">
-                <div style="color: white;">
-                    <h6 class="text-sm" style="color: white;">Company</h6>
-                    <ul class="flex-column ms-n3 nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/presentation" target="_blank" style="color: white;">
-                                About Us
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/templates/free" target="_blank">
-                                Freebies
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/templates/premium" target="_blank">
-                                Premium Tools
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/blog" target="_blank">
-                                Blog
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+
+            <!-- Panel Navigasi -->
+            <div class="col-md-3 mb-4">
+                <h6 class="fw-bold text-white text-decoration-none">Panel Navigasi</h6>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white text-decoration-none">Beranda</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Tentang Boniaga</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Jelajah Produk</a></li>
+                </ul>
             </div>
-            <div class="col-md-2 col-sm-6 col-6 mb-4">
-                <div>
-                    <h6 class="text-sm">Resources</h6>
-                    <ul class="flex-column ms-n3 nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://iradesign.io/" target="_blank">
-                                Illustrations
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/bits" target="_blank">
-                                Bits & Snippets
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/affiliates/new" target="_blank">
-                                Affiliate Program
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+
+            <!-- Alamat -->
+            <div class="col-md-3 mb-4">
+                <h6 class="fw-bold text-white text-decoration-none">Alamat</h6>
+                <p class="mb-1">Sigumpar Bar., Kec.</p>
+                <p class="mb-1">Sigumpar, Toba,</p>
+                <p class="mb-1">Sumatera Utara</p>
             </div>
-            <div class="col-md-2 col-sm-6 col-6 mb-4">
-                <div>
-                    <h6 class="text-sm">Help & Support</h6>
-                    <ul class="flex-column ms-n3 nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/contact-us" target="_blank">
-                                Contact Us
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/knowledge-center" target="_blank">
-                                Knowledge Center
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://services.creative-tim.com/?ref=ct-mk2-footer"
-                                target="_blank">
-                                Custom Development
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/sponsorships" target="_blank">
-                                Sponsorships
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+
+            <!-- Kontak -->
+            <div class="col-md-3 mb-4">
+                <h6 class="fw-bold text-white text-decoration-none">HP/WhatsApp</h6>
+                <p class="mb-1">+62 852-1391-5630</p>
             </div>
-            <div class="col-md-2 col-sm-6 col-6 mb-4 me-auto">
-                <div>
-                    <h6 class="text-sm">Legal</h6>
-                    <ul class="flex-column ms-n3 nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/knowledge-center/terms-of-service"
-                                target="_blank">
-                                Terms & Conditions
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/knowledge-center/privacy-policy"
-                                target="_blank">
-                                Privacy Policy
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.creative-tim.com/license" target="_blank">
-                                Licenses (EULA)
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-12">
-                <div class="text-center">
-                    <p class="text-dark my-4 text-sm font-weight-normal">
-                        All rights reserved. Copyright ©
-                        <script>
-                            document.write(new Date().getFullYear())
-                        </script> Boniaga by <a href="https://www.creative-tim.com" target="_blank">Group 7 PA 1 TRPL</a>.
-                    </p>
-                </div>
+        </div>
+
+        <!-- Garis Horizontal -->
+        <hr class="border-top border-light">
+
+        <!-- Copyright -->
+        <div class="row">
+            <div class="col-12 text-center">
+                <p class="mb-0 small">&copy; 2025 BONIAGA - Semua Hak Dilindungi</p>
             </div>
         </div>
     </div>
