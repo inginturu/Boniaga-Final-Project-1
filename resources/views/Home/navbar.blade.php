c71a3a6a6319ada5f936dcb854c2992d8298a2f7
Markuss5 <markusyogoseptianmanihuruk@gmail.com>
2025-05-15 14:25:00 +0700

    hi

--- resources/views/Home/navbar.blade.php
+++ resources/views/Home/navbar.blade.php
@@ -1,17 +1,14 @@
-<<<<<<< Updated upstream
 <nav class="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100 shadow-none my-3  navbar-transparent ">
     <div class="container" style="background-color: #B53535; ">
         <a class="navbar-brand d-flex align-items-center text-white" href="/">
             <img src="{{ url('assets/img/logo.png') }}" alt="Boniaga Logo" style="height: 50px;" class="me-2">
             <span class="fw-bold d-none d-sm-inline">Boniaga</span>
-=======
 <nav class="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100 shadow-none my-3 navbar-transparent">
     <div class="container" style="background-color: #B53535;">
         @if($informationStore)
         <a class="navbar-brand d-flex align-items-center text-white" href="/">
             <img src="{{asset('storage/' .$informationStore->logo) }}" alt="Boniaga Logo" style="height: 50px;" class="me-2">
             <span class="fw-bold d-none d-sm-inline">{{$informationStore->name}}</span>
->>>>>>> Stashed changes
         </a>
         <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
             data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
@@ -31,41 +28,9 @@
                     <a class="nav-link text-white" href="products">Produk</a>
                 </li>
                 <li class="nav-item">
-<<<<<<< Updated upstream
-                    <a class="nav-link text-white" href="tentangkami">Tentang Kami</a>
-=======
                     <a class="nav-link text-white" href="/about-us">Tentang Kami</a>
->>>>>>> Stashed changes
                 </li>
             </ul>
         </div>
     </div>
 </nav>
-
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    // Make sure hamburger button works
-    var navbarToggler = document.querySelector('.navbar-toggler');
-    
-    // Make dropdown text color black when dropdown is shown
-    var navLinks = document.querySelectorAll('.navbar-collapse .nav-link');
-    
-    // Add event listener for bootstrap collapse events
-    var navbarCollapse = document.getElementById('navigation');
-    navbarCollapse.addEventListener('show.bs.collapse', function () {
-        // Change text color to black when dropdown is shown
-        navLinks.forEach(function(link) {
-            link.classList.remove('text-white');
-            link.classList.add('text-dark');
-        });
-    });
-    
-    navbarCollapse.addEventListener('hide.bs.collapse', function () {
-        // Change text color back to white when dropdown is hidden
-        navLinks.forEach(function(link) {
-            link.classList.remove('text-dark');
-            link.classList.add('text-white');
-        });
-    });
-});
-</script>
