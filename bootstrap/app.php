13b733d06ec5dd47430d917f3888eaa75ce03b7e
barthms <andikabartolomeus677@gmail.com>
2025-08-28 13:11:20 +0700

    amen

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\URL;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,8 +12,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Memberitahu Laravel untuk mempercayai header dari proxy Railway
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+// mengatasi masalah URL di Railway
+if ($app->isProduction()) {
+    URL::forceScheme('httpss');
+}
